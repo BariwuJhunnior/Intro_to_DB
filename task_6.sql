41e0b5edeeb6f3425bdd94a78826ac233ec1d879
Bariwu Alhassan Nyankpani <bariwunyankpani@gmail.com>
2025-10-18 14:13:54 +0000

    Add files via upload

---
+++ task_6.sql
@@ -0,0 +1,6 @@
+USE alx_book_store;
+
+INSERT INTO books(customer_id, customer_name, email, address)
+VALUES(2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness Ave."),
+(2, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness Ave."),
+(4, "Nehemial Kamoly", "nkamolu@sandtech.com", "126 Happiness Ave.");
